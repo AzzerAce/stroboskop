7604a09010646fd5bd2928a926ca87e36b710248
Urban Tanko <urbantanko@gmail.com>
2016-03-20 21:44:21 +0000

    Dinamika gumba za zagon stroboskopa

--- skripta.js
+++ skripta.js
@@ -30,8 +30,8 @@ window.addEventListener('load', function() {
 		if (ustavi) {
 			ustavi = false;
 		} else {
-			novId = (id+1) % vrednosti.length;
-			timeout = Math.floor((Math.random() * (maxCas-minCas)) + minCas);
+			var novId = (id+1) % vrednosti.length;
+			var timeout = Math.floor((Math.random() * (maxCas-minCas)) + minCas);
 			setTimeout(function() {spremeniBarvo(novId)} , timeout);
 		}		
 	}
@@ -47,13 +47,13 @@ window.addEventListener('load', function() {
 	var zagon = function(event) {
 		vrednosti = [];
 		var barve = document.querySelectorAll("#barve > button");
-		for (i = 0; i < barve.length; i++) {
+		for (var i = 0; i < barve.length; i++) {
 			var barva = barve[i];
 			vrednosti.push(barva.innerHTML);
 		}
 		
-		minCas = 1000;
-		maxCas = 1000;
+		minCas = document.getElementById("min").value;
+		maxCas = document.getElementById("max").value;
 		spremeniBarvo(0);
 		
 		var start = document.querySelector("#start");
